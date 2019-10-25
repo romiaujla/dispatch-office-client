6c4762f07597c28ae749bb317ccac0cef6f40209
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-25 13:43:47 -0400

    created the create account component

---
+++ src/Components/CreateAccount/CreateAccount.js
@@ -0,0 +1,78 @@
+import React, { Component } from 'react';
+import './CreateAccount.css';
+import AppContext from '../../Contexts/AppContext';
+
+class CreateAccount extends Component {
+
+    validateUsername = (e) => {
+        const username = e.target.value;
+        
+    }
+
+    handleFormSubmit = (e) => {
+        e.preventDefault();
+        const { username, password, full_name, company_name, mc_num } = e.target
+        const user = {
+            username: username.value,
+            password: password.value,
+            company_name: company_name.value,
+            mc_num: mc_num.value,
+            full_name: full_name.value
+        }
+
+        console.log(user);
+    }
+
+    render() {
+        return (
+            <form className='CreateAccount width-wrapper' onSubmit={(e) => { this.handleFormSubmit(e) }}>
+                <fieldset>
+                    <legend className='blue-back white-text'>
+                        Create Account
+                    </legend>
+                    <div className='flex'>
+                        <label htmlFor='username'>
+                            <span className='input-title'>* Username:</span>
+                            <input
+                                type='text'
+                                id='username'
+                                name='username'
+                                required
+                                onChange={(e) => { this.validateUsername(e) }}
+                            />
+                            <span className='error'>Invalid Username</span>
+                        </label>
+                        <label htmlFor='password'>
+                            <span className='input-title'>* Password:</span>
+                            <input type='password' id='password' name='password' required />
+                            <span className='error'>Password Error</span>
+                        </label>
+                        <label htmlFor='full_name'>
+                            <span className='input-title'>* Full Name:</span>
+                            <input type='text' id='full_name' name='full_name' required />
+                            <span className='error'>Invalid Username</span>
+                        </label>
+                        <label htmlFor='company_name'>
+                            <span className='input-title'>Company Name:</span>
+                            <input type='text' id='company_name' name='company_name' required />
+                            <span className='error'>Company Name Error</span>
+                        </label>
+                        <label htmlFor='mc_num'>
+                            <span className='input-title'>MC Number:</span>
+                            <input type='text' id='mc_num' name='mc_num' />
+                            <span className='error'>MC Number Error</span>
+                        </label>
+                        <button
+                            className='app-button'
+                            type='submit'
+                        >
+                            Create Account
+                        </button>
+                    </div>
+                </fieldset>
+            </form>
+        );
+    }
+}
+
+export default CreateAccount;
