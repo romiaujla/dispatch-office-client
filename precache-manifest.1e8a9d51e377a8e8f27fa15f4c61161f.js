d75229e7dff63545156c1eeeb0565d55e61831df
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-24 20:57:53 -0400

    Updates

---
+++ precache-manifest.1e8a9d51e377a8e8f27fa15f4c61161f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aa4c48c21bfa8a33e9b1e330259709e",
+    "url": "/dispatch-office-client/index.html"
+  },
+  {
+    "revision": "886eb47446af14637e8a",
+    "url": "/dispatch-office-client/static/css/main.82b21c91.chunk.css"
+  },
+  {
+    "revision": "c1ac3aa243014620f8da",
+    "url": "/dispatch-office-client/static/js/2.14d1eeec.chunk.js"
+  },
+  {
+    "revision": "886eb47446af14637e8a",
+    "url": "/dispatch-office-client/static/js/main.78d4e4ec.chunk.js"
+  },
+  {
+    "revision": "92bdfa6a251d0a922a03",
+    "url": "/dispatch-office-client/static/js/runtime-main.7c8d3a87.js"
+  }
+]);
