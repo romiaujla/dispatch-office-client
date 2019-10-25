8b12be0548e98bd965ac6f5e24ca47f1aa5f5a63
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-25 15:08:15 -0400

    created a helper function file that will be shared in mulitple components

---
+++ src/HelperFunctions/HelperFunctions.js
@@ -0,0 +1,9 @@
+export function validateUserName(e){
+    const username = e.target.value;
+    console.log(username);
+}
+
+export function validatePassword(e){
+    const password = e.target.value;
+    console.log(password);
+}
