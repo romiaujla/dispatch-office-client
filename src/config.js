4ac497d0050c0d8fde42edacda17dd576d161555
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-26 10:52:12 -0400

    added token_key in config

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
     API_ENDPOINT: process.env.API_ENDPOINT || 'http://localhost:8000/api',
-    TOKEN_KEY: 'dispatch-office-auth-key',
+    TOKEN_KEY: process.env.TOKEN_KEY || '9e7d73be-f7f1-11e9-8f0b-362b9e155667',
 }
