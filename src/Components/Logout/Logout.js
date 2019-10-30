702afbd882a965e8f029505a995cc30f2af8a87e
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-29 21:15:27 -0400

    on page refresh if user is logged in the state refreshes

--- src/Components/Logout/Logout.js
+++ src/Components/Logout/Logout.js
@@ -2,10 +2,35 @@ import React, {Component} from 'react';
 import AppContext from '../../Contexts/AppContext';
 import './Logout.css';
 import TokenService from '../../Services/TokenService';
+import CarrierServices from '../../Services/CarrierServices';
 
 class Logout extends Component {
 
     static contextType = AppContext;
+
+    getCarrierInfoInComponent = async () => {
+        const carrier = await CarrierServices.getCarrierInfo()
+            .then(res => {
+                this.context.setLoggedInCarrier(res);
+                return res;
+            })
+            .catch(error => error);
+        this.setState({
+            carrier: carrier[0]
+        })
+    }
+
+    constructor(props){
+        super(props);
+        this.state = {
+            carrier: {}
+        }
+        this.getCarrierInfoInComponent()
+    }
+
+    state = {
+        carrier: {}
+    }
 
     handleLogout = () => {
         this.context.setLoggedIn(false);
@@ -13,11 +38,13 @@ class Logout extends Component {
         this.props.history.push('/');
     }
 
+    
+
     render(){
         return (
             <div className='Logout width-wrapper'>
                 Welcome, <span className='carrier-name red-text'>
-                    Username
+                    {this.state.carrier.full_name}
                 </span>!! <button className='app-link' onClick={() => {this.handleLogout()}}>Logout</button>
             </div>
         )
