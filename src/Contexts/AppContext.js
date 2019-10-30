7b42a005930cd588e93a59b8969eabad77f31410
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-29 21:15:41 -0400

    added context to hold carrier data

--- src/Contexts/AppContext.js
+++ src/Contexts/AppContext.js
@@ -7,4 +7,7 @@ export default React.createContext({
     setLoggedIn: () => {},
     setNewUser: () => {},
     setCarrier: () => {},
+    loggedInCarrier: {},
+    carrier: [],
+    setLoggedInCarrier: () => {},
 });
