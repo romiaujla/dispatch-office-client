f65154525be999099bf157a41ea9e8a796a0944b
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-10-29 21:15:50 -0400

    added service to get carrier info

--- src/Services/CarrierServices.js
+++ src/Services/CarrierServices.js
@@ -3,7 +3,6 @@ import config from '../config';
 
 const CarrierService = {
     getCarrierData(){
-        console.log(TokenService.getAuthToken());
         return fetch(`${config.API_ENDPOINT}/carrier`, {
             method: 'GET',
             headers: {
@@ -14,6 +13,22 @@ const CarrierService = {
         .then(res => res.json())
         .then(carrierData => {
             return carrierData;
+        })
+        .catch(err => {
+            console.log(err);
+        })
+    },
+    getCarrierInfo(){
+        return fetch(`${config.API_ENDPOINT}/carrier-info`, {
+            method: 'GET',
+            headers: {
+                "content-type": "application/json",
+                "Authorization" : `bearer ${TokenService.getAuthToken()}`
+            }
+        })
+        .then(res => res.json())
+        .then(carrierInfo =>  {
+            return carrierInfo;
         })
         .catch(err => {
             console.log(err);
