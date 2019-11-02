b39a2fa77a03f18a7ef7e5bb4cfd93efdc86fa11
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-11-02 13:53:42 -0400

    added validate date and format date functions

--- src/HelperFunctions/HelperFunctions.js
+++ src/HelperFunctions/HelperFunctions.js
@@ -10,6 +10,22 @@ export function validatePassword(e){
     // Write code for validating Password
 }
 
+export function validateDate(date){
+    const date_regex = /^(0?[1-9]|1[0-2])\/(0?[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/ ;
+    return date.length < 8 || !date_regex.test(date);
+}
+
+// takes user back to the previous page in history
 export function handleGoBack(history){
     return history.goBack();
 }
+
+
+// Returns date in a formatted manner
+export function formatDate(dbDate){
+    const date = new Date(dbDate);
+    const day = date.getDate();
+    const month = date.getMonth();
+    const year = date.getFullYear();
+    return `${month + 1}/${day}/${year}`;
+};
