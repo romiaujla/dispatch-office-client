d318a68f4f7dae4a3459dcf21107037abbfcb9c5
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-11-02 13:52:34 -0400

    refactored driver drop down to populate using drivers array

--- src/Components/DriversDropDown/DriversDropDown.js
+++ src/Components/DriversDropDown/DriversDropDown.js
@@ -3,37 +3,39 @@ import './DriversDropDown.css'
 
 class DriversDropDown extends Component {
     
+    constructor(props){
+        super(props)
+        this.state = {
+            optionsValue: props.defaultValue
+        }
+    }
+    
     static defaultProps = {
         drivers: [],
-        defaultValue: -1,
-        className: 'select-css'
+        defaultValue: 0,
+        className: 'select-css',
+        name: 'driver',
+        id: 'driver'
     }
 
-    renderDriverOptions = (drivers, defaultValue) => {
+    handleChange = (e) => {
+        this.setState({
+            optionsValue: e.target.value
+        })
+    }
+
+    renderDriverOptions = (drivers) => {
         if(drivers !== []){
-            return drivers.map(driver => {
-                console.log(`currentDriver`, defaultValue);
-                console.log(`driver.id`, driver.id)
-                if(defaultValue === driver.id){
-                    return (
-                        <option 
-                            value={driver.id}
-                            key={driver.id}
-                            defaultValue
-                        >
-                            {driver.full_name}
-                        </option>
-                    )
-                }else{
-                    return (
-                        <option 
-                            value={driver.id}
-                            key={driver.id}
-                        >
-                            {driver.full_name}
-                        </option>
-                    )
-                }
+            return drivers.map((driver, i) => {
+                return (
+                    <option 
+                        value={driver.id}
+                        key={i}
+                        defaultValue={true}
+                    >
+                        {driver.full_name}
+                    </option>
+                )
             })
         }
         
@@ -41,19 +43,18 @@ class DriversDropDown extends Component {
 
     render() {
 
-        const { className = 'select-css', drivers = [], defaultValue = -1} = this.props;
-        
+        const { className = 'select-css', drivers = [], name='driver', id='driver'} = this.props;
+
+
         return (
             <div className='DriversDropDown'>
-                <select className={className}>
-                    {
-                        defaultValue !== -1 
-                        ?
-                            <option value='no-driver'>No Driver</option>
-                        : 
-                            <option value='no-driver' defaultValue>No Driver</option>
-                    }
-                    {this.renderDriverOptions(drivers, defaultValue)}    
+                <select className={className}
+                    name={name}
+                    id={id} 
+                    value={this.state.optionsValue} 
+                    onChange={(e) => {this.handleChange(e)}}>
+                    <option value='-1'>No Driver</option>
+                    {this.renderDriverOptions(drivers)}    
                 </select>
             </div>
         );
