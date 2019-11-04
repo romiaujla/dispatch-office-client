bc893d8ec1c6282651a037056238ecde103d8a6f
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-11-04 08:09:24 -0500

    created the driver page

--- src/Routes/DriversPage/DriversPage.js
+++ src/Routes/DriversPage/DriversPage.js
@@ -1,13 +1,38 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './DriversPage.css';
+import AppContext from '../../Contexts/AppContext';
+import DriverCard from '../../Components/DriverCard/DriverCard';
 
-class DriversPage extends Component{
+class DriversPage extends Component {
 
-    
-    render(){
+    static contextType = AppContext
+
+
+    renderDrivers = (drivers) => {
+        
+        return drivers.map((driver) => {
+            if(driver.status === 'active')
+                return (
+                    <li key={driver.id}>
+                        <DriverCard driver={driver} />
+                    </li>
+                )
+            
+        })
+    }
+
+    render() {
+
+        const { drivers }= this.context
+
         return (
             <section className='DriversPage width-wrapper'>
-                DriversPage
+                <h4 className='blue-back white-text'>
+                    Drivers List
+                </h4>
+                <ul className='drivers-list'>
+                    {this.renderDrivers(drivers)}
+                </ul>
             </section>
         )
     }
