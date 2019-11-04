2e5cd9d40f7efcae8502404494ac75b96b222240
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-11-04 12:23:14 -0500

    Updates

---
+++ precache-manifest.afa63f0b4b78e276b986e052559a8125.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f32efbef54ed138f45a6539f1756d08d",
+    "url": "/dispatch-office-client/index.html"
+  },
+  {
+    "revision": "9a26f66d6d38f6f1081e",
+    "url": "/dispatch-office-client/static/css/main.a787fcfe.chunk.css"
+  },
+  {
+    "revision": "22158be6228ba23904d8",
+    "url": "/dispatch-office-client/static/js/2.8f5d12f8.chunk.js"
+  },
+  {
+    "revision": "9a26f66d6d38f6f1081e",
+    "url": "/dispatch-office-client/static/js/main.67819b5b.chunk.js"
+  },
+  {
+    "revision": "92bdfa6a251d0a922a03",
+    "url": "/dispatch-office-client/static/js/runtime-main.7c8d3a87.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/dispatch-office-client/precache-manifest.59ca2be2989cc50409459ec5ed6bb025.js"
+  "/dispatch-office-client/precache-manifest.afa63f0b4b78e276b986e052559a8125.js"
 );
 
 self.addEventListener('message', (event) => {
