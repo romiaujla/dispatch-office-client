40e50c74db6b2dd55abfd28845dcbc15462ae18c
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-11-04 08:08:47 -0500

    added idleEquipoments

--- src/Contexts/AppContext.js
+++ src/Contexts/AppContext.js
@@ -15,6 +15,7 @@ export default React.createContext({
     shipments: [],
     setLoggedInCarrier: () => {},
     idleDrivers: [],
+    idleEquipments: [],
     equipments: [],
     drivers: [],
     loadStatus: [
