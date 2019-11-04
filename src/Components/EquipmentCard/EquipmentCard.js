f6b32c1b898ed7a218ae8e048f4c5a6debf9ab74
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-11-04 08:08:40 -0500

    added handledelete and correct swaping equipments

--- src/Components/EquipmentCard/EquipmentCard.js
+++ src/Components/EquipmentCard/EquipmentCard.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import './EquipmentCard.css';
-import {Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import config from '../../config';
+import AppContext from '../../Contexts/AppContext';
+import {
+    arrayIsEmpty
+} from '../../HelperFunctions/HelperFunctions'
 
 class EquipmentCard extends Component {
 
@@ -12,17 +16,37 @@ class EquipmentCard extends Component {
         }
     }
 
-    handleEditButton = (equipment) => {
-        this.setState({
-            editingMode: true
+    static contextType = AppContext
+
+    handleDeleteEquipment = (id) => {
+
+        let { equipments } = this.context
+        const { drivers, idleDrivers } = this.context
+        equipments = equipments.filter((equipment) => equipment.id !== id)
+        drivers.map(driver => {
+            if (driver.equipment.id === id) {
+                driver.equipment = {}
+            }
         })
-        console.log(`Entered editing function`, equipment);
+        idleDrivers.map(driver => {
+            if (driver.equipment.id === id) {
+                driver.equipment = {}
+            }
+        })
+
+        this.context.setEquipments(equipments);
+        this.context.setDrivers(drivers);
+        this.context.setIdleDrivers(idleDrivers);
+
     }
+
     render() {
 
         const { equipment } = this.props;
-
-
+        const { idleEquipments } = this.context;
+        // console.log(`equipment.id`, equipment.id, '\n');
+        const busyEquipment = arrayIsEmpty(idleEquipments.filter((idleEquipment) => idleEquipment.id === equipment.id))
+        
         return (
             <div className='EquipmentCard grey-back blue-text'>
 
@@ -40,14 +64,25 @@ class EquipmentCard extends Component {
                         </div>
                     }
                 </div>
-                <div className='equipment-buttons'>
-                    <Link className='app-button' to={`${config.BASEPATH}/equipments/edit/${equipment.id}`}>
-                        Edit
-                    </Link>
-                    <button className='app-button'>
-                        Delete
-                    </button>
-                </div>
+                {
+                    (!busyEquipment || !equipment.driver.hasOwnProperty('id')) 
+                    ?
+                    <div className='equipment-buttons'>
+                        <Link className='app-button' to={`${config.BASEPATH}/equipments/edit/${equipment.id}`}>
+                            Edit
+                        </Link>
+                        <button className='app-button' onClick={() => { this.handleDeleteEquipment(equipment.id) }}>
+                            Delete
+                        </button>
+                    </div>
+                    :
+                    <div className='equipment-buttons'>
+                        <Link className='app-button' to={`${config.BASEPATH}/equipments/edit/${equipment.id}`}>
+                            View Current Load
+                        </Link>
+                    </div>
+                }
+
 
 
             </div>
