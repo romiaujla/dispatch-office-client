47e79a9b6bdd9c9bd7612aab6381baf26c131235
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-11-03 20:51:19 -0500

    added sort method to helper function

--- src/HelperFunctions/HelperFunctions.js
+++ src/HelperFunctions/HelperFunctions.js
@@ -1,20 +1,3 @@
-export function validateUserName(e){
-    // const username = e.target.value;
-    
-    // write code for validating username
-}
-
-export function validatePassword(e){
-    // const password = e.target.value;
-    
-    // Write code for validating Password
-}
-
-export function validateDate(date){
-    const date_regex = /^(0?[1-9]|1[0-2])\/(0?[1-9]|1\d|2\d|3[01])\/(19|20)\d{2}$/ ;
-    return date.length < 8 || !date_regex.test(date);
-}
-
 // takes user back to the previous page in history
 export function handleGoBack(history){
     return history.goBack();
@@ -37,4 +20,8 @@ export function formatDate(dbDate){
     const month = date.getMonth();
     const year = date.getFullYear();
     return `${month + 1}/${day}/${year}`;
+};
+
+export function sortShipments(shipments, property){
+  return shipments.sort((a, b) => (a[property] < b[property] ? 1 : -1));
 };
