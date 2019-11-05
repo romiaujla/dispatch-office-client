7ad968f5d2504a63224e6c53755345f15f2bcea1
Romi Aujla <r.aujla9091@gmail.com>
2019-11-05 11:31:46 -0500

    switched to heroku path for back end

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
+const localPath = 'http://localhost:8000/api';
+const herokuPath = 'https://rocky-waters-07125.herokuapp.com/api';
+
 export default {
-    API_ENDPOINT: process.env.API_ENDPOINT || 'http://localhost:8000/api',
+    API_ENDPOINT: process.env.API_ENDPOINT || herokuPath,
     TOKEN_KEY: process.env.TOKEN_KEY || '9e7d73be-f7f1-11e9-8f0b-362b9e155667',
     BASEPATH: '/dispatch-office-client',
 }
-
-// https://rocky-waters-07125.herokuapp.com/api
