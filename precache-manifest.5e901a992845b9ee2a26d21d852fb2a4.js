20ae3b0c7b386c8cba624d4abe4eb2ad11dc12c0
Romi Aujla <r.aujla9091@gmail.com>
2019-11-06 17:32:41 -0500

    Updates

---
+++ precache-manifest.5e901a992845b9ee2a26d21d852fb2a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c57e2b9721f8c86ade511472c7057203",
+    "url": "/dispatch-office-client/index.html"
+  },
+  {
+    "revision": "efbab67ce8cc4216399b",
+    "url": "/dispatch-office-client/static/css/main.7c4db8e4.chunk.css"
+  },
+  {
+    "revision": "c4c6d7998b17a3224cde",
+    "url": "/dispatch-office-client/static/js/2.1fb77668.chunk.js"
+  },
+  {
+    "revision": "efbab67ce8cc4216399b",
+    "url": "/dispatch-office-client/static/js/main.a4588cbb.chunk.js"
+  },
+  {
+    "revision": "92bdfa6a251d0a922a03",
+    "url": "/dispatch-office-client/static/js/runtime-main.7c8d3a87.js"
+  }
+]);
