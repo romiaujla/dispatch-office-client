82471ca87f66a4e45f687f8666f0163d1327ab41
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-11-05 22:33:54 -0500

    created a file to work on for shipments service

---
+++ src/Services/ShipmentsService.js
@@ -0,0 +1,10 @@
+import TokenService from "./TokenService";
+import config from "../config";
+
+const ShipmentsSerivce = {
+    updateShipment(){
+        // do stuff here 
+    }
+}
+
+export default ShipmentsSerivce;
