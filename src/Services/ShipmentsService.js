ed9a1e57bb5d225c2515cd90c6df39286447f6cf
Romi Aujla <r.aujla9091@gmail.com>
2019-11-06 15:21:40 -0500

    implemented insertShipment Service

--- src/Services/ShipmentsService.js
+++ src/Services/ShipmentsService.js
@@ -2,6 +2,35 @@ import TokenService from "./TokenService";
 import config from "../config";
 
 const ShipmentsSerivce = {
+    insertShipment(shipment){
+        return fetch(`${config.API_ENDPOINT}/shipments`, {
+            method: "POST",
+            headers: {
+              "content-type": "application/json",
+              Authorization: `bearer ${TokenService.getAuthToken()}`
+            },
+            body: JSON.stringify({
+                rate: shipment.rate || 0.0, 
+                status: shipment.status, 
+                miles: shipment.miles || 0, 
+                driver_id: shipment.driver_id || null, 
+                broker: shipment.broker || null,
+                pickup_date: shipment.pickup_date,
+                delivery_date: shipment.delivery_date,
+                pickup_city: shipment.pickup_city,
+                pickup_state: shipment.pickup_state,
+                pickup_zipcode: shipment.pickup_zipcode,
+                delivery_city: shipment.delivery_city,
+                delivery_state: shipment.delivery_state,
+                delivery_zipcode: shipment.delivery_zipcode
+            })
+          })
+            .then(response => response.json())
+            .then(shipment => shipment)
+            .catch(error => {
+              console.log(error);
+            });
+    },
     updateShipment(){
         // do stuff here 
     }
