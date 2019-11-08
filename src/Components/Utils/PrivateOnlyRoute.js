13c9f5d1144afca2087f2afed398ab3914eb968a
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-11-08 10:44:24 -0500

    added utils for public and private routes

---
+++ src/Components/Utils/PrivateOnlyRoute.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Route, Redirect } from 'react-router-dom'
+import TokenService from '../../services/token-service'
+import config from '../../config';
+
+export default function PrivateRoute({ component, ...props }) {
+  const Component = component
+  return (
+    <Route
+      {...props}
+      render={componentProps => (
+        TokenService.hasAuthToken()
+          ? <Component {...componentProps} />
+          : <Redirect
+              to={{
+                pathname: `${config.BASEPATH}/login`,
+              }}
+            />
+      )}
+    />
+  )
+}
