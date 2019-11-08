65eeaaaccca38b6d09f711d81f3b5ad1b9cee4b5
Raman Aujla <ramanpreet.aujla@gmail.com>
2019-11-07 19:24:41 -0500

    changed style to foucs with keyboard

--- src/Components/DesktopMenu/DesktopMenu.js
+++ src/Components/DesktopMenu/DesktopMenu.js
@@ -1,73 +1,13 @@
 import React, { Component } from 'react';
 import './DesktopMenu.css';
 import { Link } from 'react-router-dom';
-import AppContext from '../../Contexts/AppContext';
+import config from '../../config';
 
 class DesktopMenu extends Component {
 
-    static contextType = AppContext;
-
-    componentDidMount = () => {
-        this.driverAnchor.addEventListener('click', this.handleKeyboardSelect);
-        this.equipmentAnchor.addEventListener('click', this.handleKeyboardSelect);
-        this.loadAnchor.addEventListener('click', this.handleKeyboardSelect);
-        this.hasSubMenu.addEventListener('blur', this.obBlurLi);
-    }
-
-    componentWillUnmount = () => {
-        this.driverAnchor.removeEventListener('click', this.handleKeyboardSelect);
-        this.equipmentAnchor.removeEventListener('click', this.handleKeyboardSelect);
-        this.loadAnchor.removeEventListener('click', this.handleKeyboardSelect);
-        this.hasSubMenu.addEventListener('blur', this.obBlurLi);
-    }
-
-    obBlurLi = (e) => {
-        e.toElement.className = 'has-submenu';
-        console.log(`li blured`);
-    }
-
-    handleKeyboardSelect = (e) => {
-        e.preventDefault();
-
-        // Below code works but does not work when the menu looses focus.
-        
-        // if(e.toElement.outerHTML === this.driverAnchor.outerHTML){
-        //     if(this.driverAnchor.parentElement.className === 'has-submenu'){
-        //         this.driverAnchor.parentElement.className = 'has-submenu open'
-        //         this.equipmentAnchor.parentElement.className = 'has-submenu';
-        //         this.loadAnchor.parentElement.className = 'has-submenu';
-        //     }else{
-        //         this.driverAnchor.parentElement.className = 'has-submenu'
-        //     }
-        // }
-        
-        // if(e.toElement.outerHTML === this.equipmentAnchor.outerHTML){
-        //     if(this.equipmentAnchor.parentElement.className === 'has-submenu'){
-        //         this.driverAnchor.parentElement.className = 'has-submenu'
-        //         this.equipmentAnchor.parentElement.className = 'has-submenu open';
-        //         this.loadAnchor.parentElement.className = 'has-submenu';
-        //     }else{
-        //         this.equipmentAnchor.parentElement.className = 'has-submenu'
-        //     }
-        // }
-
-        // if(e.toElement.outerHTML === this.loadAnchor.outerHTML){
-        //     if(this.loadAnchor.parentElement.className === 'has-submenu'){
-        //         this.driverAnchor.parentElement.className = 'has-submenu'
-        //         this.equipmentAnchor.parentElement.className = 'has-submenu';
-        //         this.loadAnchor.parentElement.className = 'has-submenu open';
-        //     }else{
-        //         this.loadAnchor.parentElement.className = 'has-submenu'
-        //     }
-        // } 
-
-    }
-
     render() {
 
-        const { basePath } = this.context;
-
-
+        const basePath = config.BASEPATH;
 
         return (
             <nav
@@ -79,53 +19,41 @@ class DesktopMenu extends Component {
                             Dashboard
                         </Link>
                     </li>
-                    <li className="has-submenu" ref={litag => this.hasSubMenu = litag}>
-                        <a
-                            href='...'
-                            ref={(loadAnchor) => this.loadAnchor = loadAnchor}
-                            aria-haspopup="true"
-                            aria-expanded="false">
+                    <li className="has-submenu" tabIndex="0">
+                        <span>
                             Loads
-                            </a>
+                        </span>
                         <ul>
                             <li>
                                 <Link to={`${basePath}/load/new`}>Add Load</Link>
                             </li>
-                            <li>
+                            <li tabIndex="0">
                                 <Link to={`${basePath}/loads`}>View Loads</Link>
                             </li>
                         </ul>
                     </li>
-                    <li className="has-submenu">
-                        <a
-                            href='...'
-                            ref={equipmentAnchor => this.equipmentAnchor = equipmentAnchor}
-                            aria-haspopup="true"
-                            aria-expanded="false">
+                    <li className="has-submenu" tabIndex="0">
+                        <span>
                             Equipments
-                            </a>
+                        </span>
                         <ul>
-                            <li>
+                            <li tabIndex="0">
                                 <Link to={`${basePath}/equipment/new`}>Add Equipment</Link>
                             </li>
-                            <li>
+                            <li tabIndex="0">
                                 <Link to={`${basePath}/equipments`}>View Equipments</Link>
                             </li>
                         </ul>
                     </li>
-                    <li className="has-submenu">
-                        <a
-                            href='...'
-                            ref={driverAnchor => this.driverAnchor = driverAnchor}
-                            aria-haspopup="true"
-                            aria-expanded="false">
+                    <li className="has-submenu" tabIndex="0">
+                        <span>
                             Drivers
-                            </a>
+                        </span>
                         <ul>
-                            <li>
+                            <li tabIndex="0">
                                 <Link to={`${basePath}/driver/new`}>Add Driver</Link>
                             </li>
-                            <li>
+                            <li tabIndex="0">
                                 <Link to={`${basePath}/drivers`}>View Drivers</Link>
                             </li>
                         </ul>
