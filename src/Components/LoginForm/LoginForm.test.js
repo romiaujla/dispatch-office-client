e2810abea14a87f731035d42e2b504177e3f97be
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-01-13 21:25:16 -0500

    created smoke test for LoginForm Component

---
+++ src/Components/LoginForm/LoginForm.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import LoginForm from './LoginForm';
+import { BrowserRouter } from 'react-router-dom';
+
+describe(`LoginForm Component`, () => {
+  it('renders LoginForm without crashing', () => {
+    
+    const div = document.createElement('div');
+
+    ReactDOM.render(
+        <BrowserRouter> 
+            <LoginForm /> 
+        </BrowserRouter>
+    , div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+})
+
