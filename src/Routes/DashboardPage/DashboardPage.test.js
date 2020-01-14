6993b5f3b8ba8a44002688b032418ba5ac67d3c8
Raman Aujla <ramanpreet.aujla@gmail.com>
2020-01-13 21:23:55 -0500

    created smoke test for DashboardPage Component

---
+++ src/Routes/DashboardPage/DashboardPage.test.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import DashboardPage from './DashboardPage';
+import { BrowserRouter } from 'react-router-dom';
+
+describe(`DashboardPage Component`, () => {
+  it('renders DashboardPage without crashing', () => {
+    
+    const div = document.createElement('div');
+
+    ReactDOM.render(
+        <BrowserRouter> 
+            <DashboardPage /> 
+        </BrowserRouter>
+    , div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+})
+
